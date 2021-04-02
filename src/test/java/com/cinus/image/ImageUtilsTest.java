f25c1cc4da4f9448b2fa5c29b4e0ae3f3ba2c5b8
Carey Wong <cw.cinus@gmail.com>
2021-04-02 16:40:41 +0800

    ImageUtils test

--- src/test/java/com/cinus/image/ImageUtilsTest.java
+++ src/test/java/com/cinus/image/ImageUtilsTest.java
@@ -14,16 +14,14 @@ public class ImageUtilsTest {
     public void test_grayscale() throws Exception {
         BufferedImage img = ImageUtils.loadImage("src/test/java/com/cinus/image/test-data/test.jpg");
         BufferedImage bfi = ImageUtils.grayscale(img);
-        boolean ret = ImageUtils.writeImage("src/test/java/com/cinus/image/test-data/test2.jpg", ImageUtils.IMAGE_TYPE_JPG, bfi);
-        assertTrue(ret);
+        assertTrue(ImageUtils.writeImage("src/test/java/com/cinus/image/test-data/grayscale.jpg", ImageUtils.IMAGE_TYPE_JPG, bfi));
     }
 
     @Test
     public void test_dilation() throws Exception {
         BufferedImage img = ImageUtils.loadImage("src/test/java/com/cinus/image/test-data/test.jpg");
         BufferedImage bfi = ImageUtils.dilation(img, 10);
-        boolean ret = ImageUtils.writeImage("src/test/java/com/cinus/image/test-data/test3.jpg", ImageUtils.IMAGE_TYPE_JPG, bfi);
-        assertTrue(ret);
+        assertTrue(ImageUtils.writeImage("src/test/java/com/cinus/image/test-data/dilation.jpg", ImageUtils.IMAGE_TYPE_JPG, bfi));
     }
 
 
