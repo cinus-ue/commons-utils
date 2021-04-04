22cf3356580796c6f31f59a97356b0b85de19859
Carey Wong <cw.cinus@gmail.com>
2021-04-05 00:46:55 +0800

    RuntimeUtils

---
+++ src/main/java/com/cinus/runtime/ExecResult.java
@@ -0,0 +1,36 @@
+package com.cinus.runtime;
+
+public class ExecResult {
+
+    private int code;
+    private String error;
+    private String result;
+
+    public ExecResult(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
---
+++ src/main/java/com/cinus/runtime/RuntimeUtils.java
@@ -0,0 +1,21 @@
+package com.cinus.runtime;
+
+import com.cinus.exception.UtilException;
+import com.cinus.io.IOUtils;
+
+public class RuntimeUtils {
+
+    public static ExecResult excuteCmd(String sh) {
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec(sh);
+            int exitCode = process.waitFor();
+            ExecResult result = new ExecResult(exitCode);
+            result.setError(IOUtils.read(process.getErrorStream()));
+            result.setResult(IOUtils.read(process.getInputStream()));
+            return result;
+        } catch (Throwable t) {
+            throw new UtilException(t);
+        }
+    }
+}
