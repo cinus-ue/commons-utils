b9bb977892cc5c26d8320ec7527fb1978ce2bdcb
Carey Wong <cw.cinus@gmail.com>
2021-04-04 19:55:22 +0800

    Constants interface

---
+++ src/main/java/com/cinus/thirdparty/Constants.java
@@ -0,0 +1,57 @@
+package com.cinus.thirdparty;
+
+public interface Constants {
+
+    String FILE_SEP = "/";
+    String MS = "ms";
+
+    String TRUE_STR_VALUE = "1";
+
+    String FALSE_STR_VALUE = "0";
+
+    String ZERO = "0";
+
+    String BLANK = " ";
+    String EMPTY = "";
+    String ENTER = "\n";
+    String STAR = "*";
+    String AT = "@";
+    String DOLLAR = "$";
+    String TAB_SPACE = "    ";
+    String TAB = "\t";
+
+    String COMMA = ",";
+    String STOP_EN = ".";
+    String STOP_ZH = "。";
+    String QUOTE = "\"";
+    String WELL = "#";
+    String QUESTION_MARK = "?";
+    String AND_MARK = "&";
+
+
+    String SEMICOLON = ";";
+    String COLON = ":";
+    String EXCLAMATION = "!";
+    String EQUAL = "=";
+    String SINGLE_QUOTA = "'";
+    String UNDER_LINE = "_";
+    String MID_LINE = "-";
+    String ELLIPSIS = "...";
+
+    String LEFT_PARENTHESE = "(";
+    String RIGHT_PARENTHESE = ")";
+    char LEFT_PARENTHESE_CHAR = '(';
+    char RIGHT_PARENTHESE_CHAR = ')';
+    String LEFT_BRACKET = "[";
+    String RIGHT_BRACKET = "]";
+    String LEFT_BRACE = "{";
+    String RIGHT_BRACE = "}";
+    String PERCENT_SIGN = "%";
+
+    String MAIL_REGEX = "^([a-zA-Z0-9]+[_|\\_|\\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\\_|\\.]?)*[a-zA-Z0-9]+\\.[a-zA-Z]{2,4}$";
+
+    String STAND_REGEX = "^[a-zA-Z_0-9]+$";
+
+    String SPECIAL_CHARACTERS_REGEX = "^[^!|&=\\\\.*]+$";
+
+}
